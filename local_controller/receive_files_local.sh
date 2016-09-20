313436566fcbb2298c7d3af0ff521cb8893e1a5c
Julius Wang <juliuswang0728@gmail.com>
2016-09-20 17:39:22 +0300

    minor change

---
+++ local_controller/receive_files_local.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#TRANSFERDIR="/home/ubuntu/video/test_cam/"
+TRANSFERDIR="/home/amin/ClionProjects/jc_people_counting/videos/"
+TRANSFERCSV="sent.csv"
+BACKUPDIR="/home/amin/ClionProjects/jc_people_counting/videos/backup/"
+DETECTOR="/home/amin/ClionProjects/jc_people_counting/build/jc_pilot"
+LICENSE="f187f8d8b27f424fbab9064c18a7e0e4"
+#URL="0.0.0.0"
+URL="DEBUG"
+ID="1"
+ROI="0.1 0.1 1.0 1.0"
+
+python receive_files.py --transferdir $TRANSFERDIR --transfercsv $TRANSFERCSV --backupdir $BACKUPDIR --detector $DETECTOR --license $LICENSE --id $ID --url $URL -R $ROI
