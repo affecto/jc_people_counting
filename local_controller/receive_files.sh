71c993a4f03fb2b045ad8c4d71e5369c19ad841e
Ubuntu <ubuntu@ip-172-31-24-91.eu-central-1.compute.internal>
2016-09-29 08:17:14 +0000

    New start scripts

--- local_controller/receive_files.sh
+++ local_controller/receive_files.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #TRANSFERDIR="/home/ubuntu/video/test_cam/"
-TRANSFERDIR="/home/amin/ClionProjects/jc_people_counting/videos/"
+TRANSFERDIR="/data/video/cam2/"
 TRANSFERCSV="sent.csv"
-BACKUPDIR="/home/amin/ClionProjects/jc_people_counting/videos/backup/"
-DETECTOR="/home/amin/ClionProjects/jc_people_counting/build/jc_pilot"
-LICENSE="f187f8d8b27f424fbab9064c18a7e0e4"
+BACKUPDIR="/home/ubuntu/jc_people_counting/videos/backup/"
+DETECTOR="/home/ubuntu/jc_people_counting/build/jc_pilot"
+LICENSE=""
 #URL="0.0.0.0"
 URL="DEBUG"
 ID="1"
-ROI_VLINES="0.1 0.25 0.9 1.0"
-
-python receive_files.py --transferdir $TRANSFERDIR --transfercsv $TRANSFERCSV --backupdir $BACKUPDIR --detector $DETECTOR --license $LICENSE --id $ID --url URL -R $ROI_VLINES
+ROI_VLINES="0.1 0.2 0.7 1.0"
+ROI_DETECTOR="0.05 0.25 0.97 0.85"
+python receive_files.py --transferdir $TRANSFERDIR --transfercsv $TRANSFERCSV --backupdir $BACKUPDIR --detector $DETECTOR --license $LICENSE --id $ID --url URL -R $ROI_VLINES --roi $ROI_DETECTOR
